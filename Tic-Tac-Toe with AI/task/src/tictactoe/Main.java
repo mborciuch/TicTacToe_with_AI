76e9157f11a35b3238cf9638ab309a8f9e991c8f
piotr <piotrborciuch@gmail.com>
2019-06-22 12:51:50 +0200

    Second Stage Commit

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -1,24 +1,47 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        char[][] tictactoeArray = new char[3][3];
-        tictactoeArray[0][0] = 'X';
-        tictactoeArray[0][1] = 'X';
-        tictactoeArray[0][2] = 'X';
-        tictactoeArray[1][0] = 'X';
-        tictactoeArray[1][1] = 'X';
-        tictactoeArray[1][2] = 'X';
-        tictactoeArray[2][0] = 'X';
-        tictactoeArray[2][1] = 'X';
-        tictactoeArray[2][2] = 'X';
 
-        for(char tictacRow[] :  tictactoeArray){
-            for (char tictacElement : tictacRow){
-                System.out.print(tictacElement);
-            }
-            System.out.println();
+        System.out.println("Submit data in format: \"_________\"");
+        String input = scanner.nextLine();
+
+        char[][] board = prepareBoard(input);
+        printState(board);
+    }
+
+    private static char[][] prepareBoard(String input) {
+
+        char[][] board = new char[3][3];
+
+        String inputData = input.substring(1, input.length() - 1);
+
+        int resultFromDivision;
+        int restFromDivision;
+
+        for (int i = 0; i < inputData.length(); i++) {
+            resultFromDivision = i / 3;
+            restFromDivision = i % 3;
+            board[resultFromDivision][restFromDivision] = inputData.charAt(i);
         }
+        return board;
+    }
 
+    private static void printState(char[][] board) {
+
+        System.out.println("---------");
+        for (char[] boardRow : board) {
+            System.out.print("| ");
+            for (char boardElement : boardRow) {
+                System.out.print(boardElement + " ");
+            }
+            System.out.print("| \n");
+        }
+        System.out.println("---------");
     }
 }
